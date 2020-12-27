cff56afcc63255991ceaeb6d14ac908462338850
Aaron Roh <roharon98@gmail.com>
2020-12-27 16:45:15 +0900

    Feat: redis utils

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/roharon/rpdly-go-url
 go 1.15
 
 require (
+	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
+	github.com/alicebob/miniredis v2.5.0+incompatible
+	github.com/go-redis/redis/v8 v8.4.4
 	github.com/golang/protobuf v1.4.3
+	github.com/gomodule/redigo v1.8.3 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/yuin/gopher-lua v0.0.0-20200816102855-ee81675732da // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
 )
---
+++ utils/redis_conn.go
@@ -0,0 +1,54 @@
+package redisutils
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"os"
+)
+
+var ctx = context.Background()
+
+func RedisClient(addr string, password string) *redis.Client {
+	if addr == "" && password == "" {
+		_ = godotenv.Load("../.env")
+		addr = os.Getenv("redis_address")
+		password = os.Getenv("redis_password")
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       0,
+	})
+
+	return rdb
+}
+
+func Get(rdb *redis.Client, key string) (string, error) {
+	val, err := rdb.Get(ctx, key).Result()
+
+	if err == redis.Nil {
+		log.Printf("%s does not exist", key)
+		err := status.Errorf(codes.NotFound, "%s does not exist", key)
+		return "", err
+	} else if err != nil {
+		// error occurs
+		return "", err
+	}
+
+	return val, nil
+}
+
+func Set(rdb *redis.Client, key string, value string) error {
+	err := rdb.Set(ctx, key, value, 0).Err()
+
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
